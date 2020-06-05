e007d39e330914e4b53117c1b52b2da443b5fc44
yaozhendong <yaozhendong@hualala.com>
2020-06-05 09:14:21 +0800

    调整websocket测试页

--- vertx-learn/01-vertx-websocket-client/src/main/java/com/yzd/vertx/websocket/server/WebsocketServer.java
+++ vertx-learn/01-vertx-websocket-client/src/main/java/com/yzd/vertx/websocket/server/WebsocketServer.java
@@ -41,43 +41,50 @@ public class WebsocketServer extends AbstractVerticle {
   }
 
   private String getWsHtml(int port) {
-    String wsHtml = "<html>\n" +
-      "<head><title>Web Socket Test</title></head>\n" +
+    String wsHtml = "\n" +
+      "<html><head><title>Web Socket Test</title></head>\n" +
       "<body>\n" +
-      "<script>\n" +
-      "    var socket;\n" +
-      "    if (window.WebSocket) {\n" +
-      "        socket = new WebSocket(\"ws://localhost:" + port + "/myapp\");\n" +
-      "        socket.onmessage = function (event) {\n" +
-      "            alert(\"Received data from websocket: \" + event.data);\n" +
-      "        }\n" +
-      "        socket.onopen = function (event) {\n" +
-      "            alert(\"Web Socket opened!\");\n" +
-      "        };\n" +
-      "        socket.onclose = function (event) {\n" +
-      "            alert(\"Web Socket closed.\");\n" +
-      "        };\n" +
-      "    } else {\n" +
-      "        alert(\"Your browser does not support Websockets. (Use Chrome)\");\n" +
-      "    }\n" +
+      "<script type=\"text/javascript\">\n" +
+      "  var socket;\n" +
+      "  if (!window.WebSocket) {\n" +
+      "    window.WebSocket = window.MozWebSocket;\n" +
+      "  }\n" +
+      "  if (window.WebSocket) {\n" +
+      "    socket = new WebSocket(\"ws://localhost:8080/websocket\");\n" +
+      "    socket.onmessage = function(event) {\n" +
+      "      var ta = document.getElementById('responseText');\n" +
+      "      ta.value = ta.value + '\\n' + event.data\n" +
+      "    };\n" +
+      "    socket.onopen = function(event) {\n" +
+      "      var ta = document.getElementById('responseText');\n" +
+      "      ta.value = \"Web Socket opened!\";\n" +
+      "    };\n" +
+      "    socket.onclose = function(event) {\n" +
+      "      var ta = document.getElementById('responseText');\n" +
+      "      ta.value = ta.value + \"Web Socket closed\";\n" +
+      "      alert(\"Web Socket closed.\");\n" +
+      "    };\n" +
+      "  } else {\n" +
+      "    alert(\"Your browser does not support Web Socket.\");\n" +
+      "  }\n" +
       "\n" +
-      "    function send(message) {\n" +
-      "        if (!window.WebSocket) {\n" +
-      "            return;\n" +
-      "        }\n" +
-      "        if (socket.readyState == WebSocket.OPEN) {\n" +
-      "            socket.send(message);\n" +
-      "        } else {\n" +
-      "            alert(\"The socket is not open.\");\n" +
-      "        }\n" +
+      "  function send(message) {\n" +
+      "    if (!window.WebSocket) { return; }\n" +
+      "    if (socket.readyState == WebSocket.OPEN) {\n" +
+      "      socket.send(message);\n" +
+      "    } else {\n" +
+      "      alert(\"The socket is not open.\");\n" +
       "    }\n" +
+      "  }\n" +
       "</script>\n" +
       "<form onsubmit=\"return false;\">\n" +
-      "    <input type=\"text\" name=\"message\" value=\"Hello, World!\"/>\n" +
-      "    <input type=\"button\" value=\"Send Web Socket Data\" onclick=\"send(this.form.message.value)\"/>\n" +
+      "  <input type=\"text\" name=\"message\" value=\"Hello, World!\"/><input type=\"button\" value=\"Send Web Socket Data\"\n" +
+      "                                                                  onclick=\"send(this.form.message.value)\" />\n" +
+      "  <h3>Output</h3>\n" +
+      "  <textarea id=\"responseText\" style=\"width:500px;height:300px;\"></textarea>\n" +
       "</form>\n" +
       "</body>\n" +
-      "</html>";
+      "</html>\n";
     return wsHtml;
   }
 }
